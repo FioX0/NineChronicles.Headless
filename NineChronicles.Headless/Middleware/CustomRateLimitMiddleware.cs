fb10a7d8a5b6d67ac7197bd199a322a607241aae
area363 <kidonseo@gmail.com>
2023-04-19 13:22:08 +0900

    modify rate limit processor

--- NineChronicles.Headless/Middleware/CustomRateLimitMiddleware.cs
+++ NineChronicles.Headless/Middleware/CustomRateLimitMiddleware.cs
@@ -2,31 +2,37 @@ using System.IO;
 using System.Threading.Tasks;
 using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Serilog;
+using ILogger = Serilog.ILogger;
 
 namespace NineChronicles.Headless.Middleware
 {
-    public class CustomRateLimitMiddleware : IpRateLimitMiddleware
+    public class CustomRateLimitMiddleware : RateLimitMiddleware<CustomIpRateLimitProcessor>
     {
-        private readonly ILogger<CustomRateLimitMiddleware> _logger;
+        private readonly ILogger _logger;
         private readonly IRateLimitConfiguration _config;
 
         public CustomRateLimitMiddleware(RequestDelegate next,
             IProcessingStrategy processingStrategy,
             IOptions<IpRateLimitOptions> options,
             IIpPolicyStore policyStore,
-            IRateLimitConfiguration config,
-            ILogger<CustomRateLimitMiddleware> logger)
-            : base(next, processingStrategy, options, policyStore, config, logger)
+            IRateLimitConfiguration config)
+            : base(next, options?.Value, new CustomIpRateLimitProcessor(options?.Value!, policyStore, processingStrategy), config)
         {
             _config = config;
-            _logger = logger;
+            _logger = Log.Logger.ForContext<CustomRateLimitMiddleware>();
+        }
+
+        protected override void LogBlockedRequest(HttpContext httpContext, ClientRequestIdentity identity, RateLimitCounter counter, RateLimitRule rule)
+        {
+            _logger.Information($"[IP-RATE-LIMITER] Request {identity.HttpVerb}:{identity.Path} from IP {identity.ClientIp} has been blocked, quota {rule.Limit}/{rule.Period} exceeded by {counter.Count - rule.Limit}. Blocked by rule {rule.Endpoint}, TraceIdentifier {httpContext.TraceIdentifier}. MonitorMode: {rule.MonitorMode}");
         }
 
         public override async Task<ClientRequestIdentity> ResolveIdentityAsync(HttpContext httpContext)
         {
             var identity = await base.ResolveIdentityAsync(httpContext);
+            identity.ClientIp = identity.ClientIp + "/" + httpContext.Connection.RemotePort;
 
             if (httpContext.Request.Protocol == "HTTP/2")
             {
@@ -40,25 +46,14 @@ namespace NineChronicles.Headless.Middleware
                 httpContext.Request.Body.Seek(0, SeekOrigin.Begin);
                 if (body.Contains("stageTransaction"))
                 {
-                    return new ClientRequestIdentity
-                    {
-                        ClientIp = identity.ClientIp,
-                        Path = "/graphql/stagetransaction",
-                        HttpVerb = identity.HttpVerb,
-                        ClientId = identity.ClientId
-                    };
+                    identity.Path = "/graphql/stagetransaction";
                 }
 
                 return identity;
             }
 
-            return new ClientRequestIdentity
-            {
-                ClientIp = "127.0.0.1",
-                Path = "/",
-                HttpVerb = httpContext.Request.Method.ToLowerInvariant(),
-                ClientId = "anon"
-            };
+            identity.ClientIp = "127.0.0.1";
+            return identity;
         }
     }
 }
